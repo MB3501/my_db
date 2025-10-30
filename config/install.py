f5e04165860801d02b026b1b040a0eeb49af5fab
MB3501 <153351805+MB3501@users.noreply.github.com>
2025-10-30 20:43:50 +0700

    Update install.py

--- config/install.py
+++ config/install.py
@@ -19,7 +19,7 @@ data = {
     "index":["23586"],
     "comm":["config"],
     "namefile":["config.py"],
-    "version":["0.2.0"],
+    "version":["0.2.5"],
     "type":["-ao"],
     "ph":["ao"],
     "from":["python3"]
